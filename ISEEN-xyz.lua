4d2d7e758dd41f92e50a129fad564f6d9e5ba369
ShellSuwan <90244845+ShellSuwan@users.noreply.github.com>
2022-04-01 21:31:33 +0700

    Update ISEEN-xyz.lua

--- ISEEN-xyz.lua
+++ ISEEN-xyz.lua
@@ -1,8 +1,52 @@
-local main = _G.Url
-if _G.Attackh == true then
-while _G.Attackh do wait(0.5)
+if _G.Attackh2 == true then
+while _G.Attackh2 do wait(0.5)
+    
 local requestwebhook = syn.request({
-    Url = main,
+    Url = _G.Url1,
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = game:GetService("HttpService"):JSONEncode({
+		["content"] = '@here @everyone'
+	})
+})
+
+local requestwebhook = syn.request({
+    Url = _G.Url2,
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = game:GetService("HttpService"):JSONEncode({
+		["content"] = '@here @everyone'
+	})
+})
+
+local requestwebhook = syn.request({
+    Url = _G.Url3,
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = game:GetService("HttpService"):JSONEncode({
+		["content"] = '@here @everyone'
+	})
+})
+
+local requestwebhook = syn.request({
+    Url = _G.Url4,
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = game:GetService("HttpService"):JSONEncode({
+		["content"] = '@here @everyone'
+	})
+})
+
+local requestwebhook = syn.request({
+    Url = _G.Url5,
     Method = "POST",
     Headers = {
         ["Content-Type"] = "application/json"
